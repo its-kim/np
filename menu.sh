90295262a39836325fbedd0928bf2d643cfcd77a
its-kim <kimsergey91@gmail.com>
2025-01-25 18:57:18 +0300

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,7 +2,8 @@
 
 # Global Variables
 SOFTWARE_FFMPEG=(wget curl unzip build-essential libtool pkg-config cmake autoconf automake yasm gperf nasm meson python3-distutils cython3 python3-numpy)
-SOFTWARE_NP=(linux-image-generic-hwe-22.04 intel-gpu-tools aspnetcore-runtime-8.0 nginx)
+SOFTWARE_NP=(intel-gpu-tools aspnetcore-runtime-8.0  nginx)
+SOFTWARE_RUNTIME=(linux-image-generic-hwe-22.04 libxfixes3 libx11-xcb1 libxcb-dri3-0)
 FFMPEG_LIBRARIES=(lame libvpx opus fdkaac libpng libx264 libx265 libogg libvorbis libtheora dav1d openjpeg)
 LOG_FILE="build.log"
 BASE_DIR=$(dirname "$(readlink -f "$0")")
@@ -11,6 +12,16 @@ BASE_DIR=$(dirname "$(readlink -f "$0")")
 export NEWT_COLORS='
 root=white,blue
 '
+
+# Check sudo access
+check_sudo() {
+  if [[ $EUID -ne 0 ]]; then
+    echo "You need sudo rights to run this script."
+    exit 1
+  fi
+}
+
+check_sudo
 
 # Welcome Screen and EULA
 function welcome_screen {
@@ -64,10 +75,8 @@ function check_install_software_np {
 
 # 1.3 Configure Nginx
 function configure_nginx {
-    if [[ ! -d /etc/nginx/conf.d/remuxer ]]; then
-        mkdir -p /etc/nginx/conf.d/remuxer 
-        rm -f /etc/nginx/sites-available/default
-    fi
+    mkdir -p /etc/nginx/conf.d/remuxer 
+    rm -f /etc/nginx/sites-available/default
 
     cat > /etc/nginx/conf.d/np.conf <<'EOF'
 server {
@@ -86,21 +95,13 @@ server {
 EOF
 
     systemctl restart nginx
-
-    if systemctl is-active --quiet nginx; then
-        echo "Nginx restarted successfully."
-        whiptail --title "Nginx Configuration" --msgbox "Nginx has been configured and restarted successfully." 8 50
-    else
-        echo "Failed to restart Nginx. Please check the configuration."
-        whiptail --title "Nginx Configuration Error" --msgbox "Failed to restart Nginx. Check the configuration and try again." 8 50
-        exit 1
-    fi
 }
 
 # 1.4 Deploy NP
 function deploy_np_ux {
-    tar -xzf dist/np-ux.tar.gz -C /opt
-    cd /opt/np-ux && ./run.sh
+    mkdir -p /opt/noisypeak
+    tar -xzf dist/np-ux.tar.gz -C /opt/noisypeak
+    cd /opt/noisypeak/np-ux && ./run.sh
 }
 
     # 1.2 Install Standard Software
@@ -112,6 +113,7 @@ function deploy_np_ux {
     # 1.4 Deploy NP
     deploy_np_ux
 
+main_menu
 }
 
 # 2. Install FFmpeg with Custom Libraries
@@ -192,8 +194,8 @@ function install_hardware_acc {
     TARGET_KERNEL="6.8.0"
     if dpkg --compare-versions "$CURRENT_KERNEL" lt "$TARGET_KERNEL"; then
         if whiptail --title "Kernel Update Confirmation" --yesno "Your current kernel version is $CURRENT_KERNEL. Do you want to update?" 15 60; then
-            sudo apt update && sudo apt install -y linux-generic-hwe-22.04
-            whiptail --title "Reboot Confirmation" --yesno "Kernel updated successfully. Reboot now?" 15 60 && sudo reboot
+            sudo apt update && sudo apt install -y "${SOFTWARE_RUNTIME[@]}"
+            whiptail --title "Kernel Updated" --msgbox "Kernel updated successfully." 15 60
         fi
     fi
 
@@ -224,11 +226,6 @@ function install_intel_gpu_runtime {
         return 0
     fi
 
-    if [[ ! -f "$BASE_DIR/dist/MediaStack.tar.gz" ]]; then
-        whiptail --title "Missing File" --msgbox "MediaStack.tar.gz not found in the dist directory. Please ensure the file exists." 10 60
-        return 1
-    fi
-
     echo "Installing Intel GPU Runtime..." >> "$LOG_FILE"
     tar xf "$BASE_DIR/dist/MediaStack.tar.gz" -C "$BASE_DIR" >> "$LOG_FILE" 2>&1
     cd "$BASE_DIR/MediaStack" && ./install_media.sh >> "$LOG_FILE" 2>&1
@@ -253,6 +250,7 @@ function update {
         main_menu
     else
         whiptail --title "Updating" --msgbox "Updating to version $NEW_VERSION..." 8 50
+        sudo apt update && sudo apt install -y "${SOFTWARE_RUNTIME[@]}" "${SOFTWARE_FFMPEG[@]}" "${SOFTWARE_NP[@]}"
         ./build-ffmpeg.sh --ffmpeg-version "$NEW_VERSION"
         whiptail --title "Updated" --msgbox "Has been successfully updated to version $NEW_VERSION." 8 50
         main_menu
@@ -262,14 +260,16 @@ function update {
 # 5. Remove Installed Software
 function remove_all {
     if whiptail --title "Remove Software" --yesno "This will remove the following software packages:\n${SOFTWARE_FFMPEG[*]}\n${SOFTWARE_NP[*]}\nDo you want to proceed?" 20 70; then
-        sudo apt purge -y "${SOFTWARE_FFMPEG[@]}" "${SOFTWARE_NP[@]}"
+        sudo apt purge -y "${SOFTWARE_FFMPEG[@]}" "${SOFTWARE_NP[@]}" "${SOFTWARE_RUNTIME[@]}"
         sudo apt autoremove -y
         sudo apt clean
-        sudo rm -rf /opt/np-ux
+        sudo rm -rf /opt/noisypeak
         whiptail --title "Removal Complete" --msgbox "All specified software has been removed." 8 50
     else
         whiptail --title "Operation Canceled" --msgbox "The removal process has been canceled." 8 50
     fi
+
+main_menu
 }
 
 # Main Menu
